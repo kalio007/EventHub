04e520ca632953d59ec317b633c74b5f3a02d7db
kalio007 <kalioprince1@gmail.com>
2023-12-31 22:00:07 +0100

    feat: setup clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="">
-      <Button className="px-10">Hello</Button>
-    </main>
-  );
+  return <main className=""></main>;
 }
