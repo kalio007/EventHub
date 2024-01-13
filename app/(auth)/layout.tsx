22bfa2252d7d8ffa53b5724a8925742e72dc8b87
kalio007 <kalioprince1@gmail.com>
2024-01-14 00:24:41 +0100

    fixed the padding on the body

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center p-10">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
